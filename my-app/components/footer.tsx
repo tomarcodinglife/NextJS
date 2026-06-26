134636cc3822e5e08e1e59a7e18b306a65501d1e
Sujit Tomar <tomarcodinglife@gmail.com>
2026-06-26 22:34:17 +0530

    updated

---
+++ my-app/components/footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer(){
+
+  return (
+    <footer className="p-4 bg-gray-800 text-white bottom-0.5">
+      <p>
+        © 2026 TomarSoft
+      </p>
+    </footer>
+  )
+}
